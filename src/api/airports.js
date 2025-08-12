0cc6162019756958b308d25eb1278434be609483
morrisonslue <chris@morrisonslue.com>
2025-08-12 15:53:40 -0230

    feat(boards): arrivals and departures functionality from db

--- src/api/airports.js
+++ src/api/airports.js
@@ -1,6 +1,6 @@
 import client from './client'
 
 export async function listAirports() {
-  const res = await client.get('/manage/airports') 
-  return res.data
+  const res = await client.get('/airports')
+  return Array.isArray(res.data) ? res.data : []
 }
